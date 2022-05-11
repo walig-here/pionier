e9b841423ba2d2de145b81ae1dcc2ca536e16ead
Michał Bernacki-Janson <michal.bernacki-janson@outlook.com>
2022-05-11 15:58:24 +0200

    foldery dżawowe

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hellow world!");
     }
-<<<<<<< HEAD
     //Hello World
-=======
->>>>>>> origin/devlopment
+
 }
