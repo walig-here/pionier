852c57c406e0445153b84b978e6c43ccf271ef30
walig-here <daw.walig@gmail.com>
2022-05-12 18:51:35 +0200

    dodanie aktualnej tury do maina

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
-import rendering.NFrame;
+import rendering.*;
+import simulation.terrain.Field;
 
 public class Main {
 
     static private NFrame window; // główne okno symulacji
-    static private int grid_size; // rozmiar planszy(w polach)
+    static private int grid_size; // rozmiar planszy(w polach) z przedziału [???]
+    static private Field[][] map; // plansza na której odbywa się symulacja
+    static private int current_turn; // aktualna tura
 
     public static void main(String[] args) {
         grid_size = 30;
