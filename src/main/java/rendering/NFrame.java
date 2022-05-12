bd75e792ba5828075dfd83cfc034d440b9e510cb
Michał Bernacki-Janson <michal.bernacki-janson@outlook.com>
2022-05-12 16:55:08 +0200

    kanwa

--- src/main/java/rendering/NFrame.java
+++ src/main/java/rendering/NFrame.java
@@ -1,24 +1,21 @@
 package rendering;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class NFrame extends JFrame {
 
-    Kanwa1 kanwa1;
+    Kanwa1 kanwa;
     public NFrame(int sizeOfGrid){
-//sizeOfGrid musi być <=41 dla kratki wielkości 20
-        ImageIcon logo = new ImageIcon("logo.png");
-        this.setTitle("Pionier w \u015Bwiecie maszyn");
-        this.setIconImage(logo.getImage());
+
+        kanwa= new Kanwa1(sizeOfGrid);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(sizeOfGrid*20+200+15,sizeOfGrid*20+38);//maksymalna wysokość okna przy skalowaniu 125%
-        this.add(kanwa1=new Kanwa1(sizeOfGrid));
-        this.add(kanwa1.stats);
-        this.setLayout(null);
-        //this.setUndecorated(true);
-        //this.pack();
+        this.setSize(20*sizeOfGrid,20*sizeOfGrid);
+        this.add(kanwa);
+        this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
+
+
 }
