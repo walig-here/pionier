64d018af093013dc6a21e1aa144689059f9d81e7
walig-here <daw.walig@gmail.com>
2022-05-14 15:53:08 +0200

    GridSprite nie dziedziczył po Sprite a powinien

--- src/main/java/rendering/GridSprite.java
+++ src/main/java/rendering/GridSprite.java
@@ -5,7 +5,7 @@ import java.awt.*;
 /**
  * Graficzna reprezentacja pojedyńczego pola na planszy
  * */
-public class GridSprite {
+public class GridSprite extends Sprite {
 
     private Image upper_texture; // górna tekstura obiektu(wyświetlana nad główną teksturą)
 }
