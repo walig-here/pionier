9cc10a625d192c7ad952f929e388fdb2bb36a392
walig-here <daw.walig@gmail.com>
2022-05-14 14:20:51 +0200

    Skomponowanie Glitcha za maszyną zamiast polem glitcha

--- src/main/java/simulation/Glitch.java
+++ src/main/java/simulation/Glitch.java
@@ -2,12 +2,13 @@ package simulation;
 
 public abstract class Glitch {
 
-    private int range; // zasięg zakłócenia
     private float intensity; // natężenie zakłócenia(w %)
     private int glitchID; // ID glitcha
 
     public Glitch(int range, float intensity) {
-        this.range = range;
         this.intensity = intensity;
     }
+
+    // wpływ glitcha na maszyne
+    abstract void glitchImpact(Machine impacting);
 }
