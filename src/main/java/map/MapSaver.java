56301c041c69bd1ffbaec026209a95a28566c56e
Michał Bernacki-Janson <michal.bernacki-janson@outlook.com>
2022-05-22 02:51:37 +0200

    początek zapisywania map

--- src/main/java/map/MapSaver.java
+++ src/main/java/map/MapSaver.java
@@ -1,14 +1,22 @@
 package map;
 
+import rendering.MapEditorGUI;
 import simulation.terrain.Field;
+
+import javax.swing.*;
 
 public class MapSaver {
 
-    public void saveMap(Field[][] map)
+    public static void saveMap(int size)
     {
+        int[][] mapTab =new int[size][size];
+        mapTab= MapEditorGUI.getMapTab();
+
+        JFileChooser fileChooser=new JFileChooser();
+        //fileChooser.showOpenDialog()
 
     }
-    public void saveSimulation(Field[][] map)
+    public static void saveSimulation(Field[][] map)
     {
         //wywołuje wszystkie gettery i zapisuje razem z mapą do pliku
     }
